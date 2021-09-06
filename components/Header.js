f4509b2eca99451dd77e3f00083ab676528b5efa
Yiteng Huang <hyy528924543@gmail.com>
2021-09-06 15:11:07 +1000

    finish layout of header component with tailwind css

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10">
       {/* left section */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
@@ -15,10 +22,29 @@ function Header() {
       </div>
 
       {/* middle section */}
-      <div></div>
+      <div className="flex items-center  md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-6 bg-red-400 text-white rounded-full p-1 mx-2 cursor-pointer" />
+      </div>
 
       {/* right section */}
-      <div></div>
+      <div className="flex items-center justify-end space-x-3 text-gray-700">
+        <div className="p-2 cursor-pointer rounded-full hidden lg:inline-flex hover:bg-gray-300">
+          <p className="cursor-pointer">Become a host</p>
+        </div>
+        <div className="p-3 cursor-pointer rounded-full hover:bg-gray-300 ">
+          <GlobeAltIcon className="h-6" />
+        </div>
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
+        </div>
+      </div>
     </header>
   );
 }
